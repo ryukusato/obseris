dcabd4366338e123354aa597c0fa2c8fe68e5548
ryukusato <ryukusato04@gmail.com>
2025-10-27 17:03:58 +0900

    Delete Project_obseris directory

--- Project_obseris/ObserisGame/src/main/java/org/yourcompany/yourproject/config/PlayerType.java
+++
@@ -1,5 +0,0 @@
-package org.yourcompany.yourproject.config;
-public enum PlayerType {
-    HUMAN,
-    AI
-}
--- Project_obseris/ObserisGame/src/main/java/org/yourcompany/yourproject/config/SpinType.java
+++
@@ -1,6 +0,0 @@
-package org.yourcompany.yourproject.config;
-public enum SpinType {
-    NONE,
-    T_SPIN,
-    T_SPIN_MINI
-}
